af6e1da0d7376aabbe6bf25b69430390b7ffd931
Korea0706 <115077953+Korea0706@users.noreply.github.com>
2022-11-05 11:20:09 +0900

    Update ocr_v.2.py

--- ocr_v.2.py
+++ ocr_v.2.py
@@ -1,1 +1,20 @@
-s
+import os
+import my_tesseract
+from flask import Flask
+
+def print_hi(name):
+   print(f'Hi, {name}')  
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+   return_str = my_tesseract.run_tesseract(r"C:\Users\SUN\Documents\ocr test folder\눈쟁이DB.PNG")
+   return return_str
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+   print_hi('PyCharm')
+
+   app.run(port=8000)

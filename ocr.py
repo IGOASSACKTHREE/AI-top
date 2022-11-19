1f342c90efc2a91e396d64df18cc587fea1c85de
skyhaon <101443480+skyhaon@users.noreply.github.com>
2022-11-19 14:18:54 +0900

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,9 +1,17 @@
+#이미지 처리하는 pillow 라이브러리 불러오기
 from PIL import Image
+
+#Tesseract 라이브러리 불러오기
 import pytesseract
 
-image_path = r"안녕하세요.png"
+#이미지(경로) 선택
+image_path = r"이미지 경로"
 
+#Tesseract 파일 위치
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+#text 변수에 OCR 결과 저장
 text = pytesseract.image_to_string(Image.open(image_path), lang="kor")
 
+#결과 출력
 print(text)
